52bab301670b52cfdb1c5f3f8b949afc7a26db31
hexin17 <hexin17@baidu.com>
2021-12-17 18:14:48 +0800

    ADD: baiduyun build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,9 @@ module.exports = {
     apps: [
         {
             name: "iqiaoxin",
-            script: "next start",
-            instances: 2,
-            exec_mode: "cluster",
+            script: "npx next start",
+            instances: 1,
+            // exec_mode: "cluster",
             watch: ["dist"],
             ignore_watch: ["node_modules", "pages", "styles"],
             max_memory_restart: '300M',
